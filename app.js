09b20311d2649685b1ae8e5cab96d20a3600cd9d
ViniciusBelo <viniciusmendesbelo1@gmail.com>
2024-03-28 08:41:28 -0300

    Nova Branch

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ app.get('/', function (req, res) {
     res.send('Hello Geeks!');
     res.send('<br>');
 	res.sen('Nova Linha');
+    res.send('<br>');
+	res.send('enviando');
 });
 app.listen(3000, function () {
     console.log('Servidor na porta 3000!');
